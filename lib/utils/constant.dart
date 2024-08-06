f7751db525618f60f27330f2e668e4f33441a832
Ayush2006128 <74146837+Ayush2006128@users.noreply.github.com>
2024-08-06 19:18:15 +0530

    built 1

--- lib/utils/constant.dart
+++ lib/utils/constant.dart
@@ -1,3 +1,6 @@
 const Prompt = """
-explain this problem.
+explain the concept behind following problem
+and tell me where do i start to solve it
+just tell me steps to solve it and explain it
+DO NOT ANSWER THE QUESTION. 
 """;
